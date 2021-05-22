5a6bf5873108d058721c53fb54fb1bbc9e8da57f
Franz Decinal <dfranzzzzz05@gmail.com>
2021-05-23 03:43:15 +0800

    CRUD functions are working, using Firebase as storage

--- main.js
+++ main.js
@@ -1,38 +1,42 @@
-// Library Log
-const booksTotal    = document.querySelector(`#books_total`);
-const booksFinished = document.querySelector(`#books_finished`);
-const booksUnread   = document.querySelector('#books_unread');
+// Firebase configuration and initialization
+var firebaseConfig = {
+  apiKey: "AIzaSyAZ23WFMbLO7hv-3AjeeQaBN36EWFPzEvE",
+  authDomain: "library-7cff7.firebaseapp.com",
+  databaseURL: "https://library-7cff7-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "library-7cff7",
+  storageBucket: "library-7cff7.appspot.com",
+  messagingSenderId: "806283825553",
+  appId: "1:806283825553:web:9c83676d1768bcb4810a30",
+  measurementId: "G-XCSB27CF8K"
+};
 
-// Book
-// const bookTitle  = document.querySelector(`.book_title`);
-// const bookAuthor = document.querySelector(`.book_author`);
-// const bookGenre  = document.querySelector(`.book_genre`);
+firebase.initializeApp(firebaseConfig);
 
-// Button
-const btnAdd    = document.querySelector(`.add_book`);
+// Reference books collection
+var booksRef = firebase.database().ref('books');
 
-// Listed Books
+// For DOM manipulations
 const bookList = document.querySelector(`.listed_books`);
+const btnAdd   = document.querySelector(`.add_book`);
+const modal    = document.querySelector(`.modal`);
+const form     = document.querySelector(`.list_form`);
 
-// Form
-const modal       = document.querySelector(`.modal`);
-const form        = document.querySelector(`.list_form`);
-const form_title  = document.querySelector(`#form_title`);
-const form_author = document.querySelector(`#form_author`);
-const form_genre  = document.querySelector(`#form_genre`);
-const form_status = document.querySelector(`#form_status`);
-
+// Event Listeners
 window.onload = function () {  
-  // Event Listeners
-  btnAdd  .addEventListener(`click`, showForm);
   window  .addEventListener(`click`, clickExitForm);
+  btnAdd  .addEventListener(`click`, showForm);
   bookList.addEventListener(`click`, Book.deleteBook); // event bubbling concept
   bookList.addEventListener(`click`, Book.toggleStatus); // event bubbling concept
 }
 
+// Form functions
 form.addEventListener(`submit`, (e) => {
   e.preventDefault();
-  new Book(`${form_title.value}`, `${form_author.value}`, `${form_genre.value}`, `${form_status.value}`);
+  var title  = getInputVal(`form_title`);
+  var author = getInputVal(`form_author`);
+  var genre  = getInputVal(`form_genre`);
+  var status = getInputVal(`form_status`);
+  saveBook(title, author, genre, status);
   resetForm();
   exitForm();
 });
@@ -53,17 +57,37 @@ resetForm = () => {
   form.reset();
 }
 
-class Book {
-  constructor (title, author, genre, status) {
-    this.title  = title;
-    this.author = author;
-    this.genre  = genre;
-    this.status = status;
-    Book.allBooks.push(this);
-    this.createBook();
-  }
+saveBook = (title, author, genre, status) => {
+  var newBookRef = booksRef.child(`${title}`);
+  newBookRef.set({
+    Title : title,
+    Author: author,
+    Genre : genre,
+    Status: status
+  });
+}
 
-  createBook () {
+getInputVal = (id) => {
+  return document.getElementById(id).value;
+}
+
+renderBooks = (data) => {
+  document.querySelectorAll(".book").forEach(book => book.remove()) 
+  var books = data.val();
+  if (books == null) return;
+  var keys = Object.keys(books);
+  for ( var i = 0; i < keys.length; i++) {
+    var k = keys[i];
+    var title = books[k].Title;
+    var author = books[k].Author;
+    var genre = books[k].Genre;
+    var status = books[k].Status;
+    Book.createBook (`${title}`, `${author}`, `${genre}`, `${status}`);
+  }
+}
+
+class Book {
+  static createBook (title, author, genre, status) {
     const book         = document.createElement(`div`);
     const bookTitle    = document.createElement(`h4`);
     const bookAuthor   = document.createElement(`h4`);
@@ -76,13 +100,13 @@ class Book {
     bookAuthor.className = `book_author`;
     bookGenre.className  = `book_genre`;
     deleteBtn.className  = `book_delete`;
-    if (this.status === 'DONE') indicatorBtn.className = `book_done`;
-    if (this.status === 'UNREAD') indicatorBtn.className = `book_unread`;
+    if (status === 'DONE') indicatorBtn.className = `book_done`;
+    if (status === 'UNREAD') indicatorBtn.className = `book_unread`;
 
-    bookTitle.innerText    = `${this.title}`
-    bookAuthor.innerText   = `${this.author}`
-    indicatorBtn.innerText = `${this.status}`;
-    bookGenre.innerText    = `${this.genre}`
+    bookTitle.innerText    = `${title}`
+    bookAuthor.innerText   = `${author}`
+    indicatorBtn.innerText = `${status}`;
+    bookGenre.innerText    = `${genre}`
     deleteBtn.innerText    = `DELETE`;
 
     bookList.append(book);
@@ -93,55 +117,33 @@ class Book {
     book.append(deleteBtn);
   }
 
-  // event bubbling
   static toggleStatus (e) {
     // if status is DONE
     if (e.target.className == 'book_done') {
       const book  = e.target.parentElement;
-      const title = `${book.childNodes[0].innerText}`;
-      Book.allBooks.map(objInstance => objInstance.title === `${title}` ? 
-        objInstance.status = `UNREAD`: objInstance
-      )
-      e.target.classList.remove(`book_done`);
-      e.target.classList.add(`book_unread`)
-      e.target.innerText = `UNREAD`;
-      console.log(Book.allBooks);
-      console.log(e.target.className);
-      return;
+      const title = `${book.childNodes[0].innerText}`; 
+      booksRef.child(title).update({
+        Status: `UNREAD`
+      })
     }
+
     // if status is UNREAD
     if (e.target.className == 'book_unread') {
       const book  = e.target.parentElement;
-      const title = `${book.childNodes[0].innerText}`;
-      Book.allBooks.map(objInstance => objInstance.title === `${title}` ? 
-        objInstance.status = `DONE`: objInstance
-      )
-      e.target.classList.remove(`book_unread`);
-      e.target.classList.add(`book_done`);
-      e.target.innerText = `DONE`;
-      console.log(Book.allBooks);
-      console.log(e.target.className);
-      return;
+      const title = `${book.childNodes[0].innerText}`; 
+      booksRef.child(title).update({
+        Status: `DONE`
+      })
     }
   }
 
-  // event bubbling concept
   static deleteBook (e) {
     if (e.target.className == 'book_delete') {
       const book  = e.target.parentElement;
       const title = `${book.childNodes[0].innerText}`; 
-      const index = Book.allBooks.findIndex(x => x.title === title);
-      bookList.removeChild(book);
-      Book.allBooks.splice(index, 1);
+      booksRef.child(title).remove()
     }
   }
-
-  // class instance object array
-  static allBooks = [];
 }
 
-// Test Samples
-new Book (`Atomic Habits`, `James Clear`, `Self-help`, `DONE`);
-new Book (`Subtle Art of not Giving a F*ck`, `Mark Manson`, `Self-help`, `UNREAD`);
-new Book (`Naruto`, `Mark Manson`, `Self-help`, `UNREAD`);
-new Book (`Eren`, `Mark Manson`, `Self-help`, `DONE`);
+booksRef.on('value', renderBooks);
